99b201abbee5681d5054d1812f5fb8913a3ff719
mkhalfadel <fadel.mokahal@gmail.com>
2025-10-12 19:44:21 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+
+export default function App()
+{
+  return (
+      <Routes>
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
+  )
+}
